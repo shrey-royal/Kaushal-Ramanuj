5790f0529d3690a9eb234588da12923370276483
Shrey K <srkkadia@gmail.com>
2024-03-22 20:05:16 +0530

    String methods

--- Strings/One.java
+++ Strings/One.java
@@ -1,15 +1,64 @@
 public class One {
    public static void main(String[] args) {
-      String s = "Kaushal Ramanuj";
-      System.out.println(s.hashCode());   
-      s = "Kaushal";
-      System.out.println(s.hashCode());
-      String s1 = "Kaushal";
-      System.out.println(s1.hashCode());
+      // String s = "Kaushal Ramanuj";
+      // System.out.println(s.hashCode());   
+      // s = "Kaushal";
+      // System.out.println(s.hashCode());
+      // String s1 = "Kaushal";
+      // System.out.println(s1.hashCode());
 
+
+      // Ways to create a string
+      // String s1 = "declare";
+      // String s2 = new String();
+      // String s3 = new String("declare");
+
+      // String s1 = null, s = "Here is a sample string that you can use for testing various string methods. It contains uppercase and lowercase letters, punctuation marks like commas, periods, and exclamation marks! Additionally, there are spaces between words.";
+
+      //Methods
+      // System.out.println(s.charAt(12));
+      // System.out.println(s.length());
+      // System.out.println(s.substring(12));
+      // System.out.println(s.substring(12, 20));
+      // System.out.println(s.indexOf(' ', 10));
+      // System.out.println(s.indexOf("er", 100));
+      // System.out.println(s.lastIndexOf(' ', 100));
+      // System.out.println(s.startsWith("Here"));
+      // System.out.println(s.endsWith("words."));
+      // System.out.println(s.toUpperCase());
+      // System.out.println(s.toLowerCase());
+      // System.out.println("\"" + "      K A U S H A L         ".trim() + "\"");
+      // System.out.println(s.replace(' ', '_'));
+      // System.out.println(s.replace("e", "E"));
+      // System.out.println(s.contains("_"));
+      // System.out.println("".isEmpty());
       
+      // int i = 12345;
+      // System.out.println(((Object)i).getClass().getSimpleName());
+      // String s2 = String.valueOf(i);
+      // System.out.println(s2.getClass().getSimpleName());
+      
+      // int ab = Integer.valueOf(s2);
+      // System.out.println(((Object)ab).getClass().getSimpleName());
+      
+      
+      // System.out.println("Hello ".concat("World!"));
+      // s = "Hello World!";
+      // char a[] = s.toCharArray();
 
+      // for (char c : a) {
+      //    System.out.print(c + ", ");
+      // }
+      // System.out.println();
 
+      // System.out.println("Hello".equals("hello"));
+      // System.out.println("Hello".equalsIgnoreCase("hello"));
+      // System.out.println("hello".compareTo("Hello"));
+      // System.out.println("hello".compareToIgnoreCase("Hello"));
+
+      // char a[] = {'a', '_', 'b', '_', 'c', '_', 'd'};
+      // s = new String(a);
+      // System.out.println(s);
    }
 }
 
